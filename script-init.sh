512525e257c24a3f573cf73ad5dda90c6fdb1b07
Aqmar Zafrei <aqmar@Aqmars-MacBook-Air.local>
2025-11-21 17:22:08 +0800

    Update script-init.sh

--- script-init.sh
+++ script-init.sh
@@ -1,4 +1,4 @@
 curl -fsSL https://raw.githubusercontent.com.Ogawa-Kazuma/script-collection/refs/heads/main/script-cache-cabin.sh | sh
-curl -fsSL https://raw.githubusercontent.com/Ogawa-Kazuma/script-collection/refs/heads/main/install-docker.sh | sed 's/\r$//' | bash
+curl -fsSL https://raw.githubusercontent.com/Ogawa-Kazuma/script-collection/refs/heads/main/install-docker.sh | bash
 curl -fsSL https://raw.githubusercontent.com/Ogawa-Kazuma/script-collection/refs/heads/main/install-nodered-docker.sh | sed 's/\r$//' | bash
 curl https://raw.githubusercontent.com/Ogawa-Kazuma/script-collection/refs/heads/main/install-tailscale-docker.sh | sed 's/\r$//' | bash
